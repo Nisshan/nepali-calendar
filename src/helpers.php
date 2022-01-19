1e91b46811063466e4d6307c48d1181e86a78ee2
nisshan <timsinanishan1@gmail.com>
2022-01-20 00:34:42 +0545

    adding helper functions

---
+++ src/helpers.php
@@ -0,0 +1,19 @@
+<?php
+
+use Nisshan\NepaliCalendar\DateConversion;
+
+if (! function_exists('toNepaliDate')) {
+
+function toNepaliDate(int $year, int $month, int $day, string|null $separator = null) :string
+{
+   return DateConversion::convert($year, $month, $day, $separator)->toNepali();
+}
+}
+
+if (! function_exists('toEnglishDate')) {
+
+    function toEnglishDate(int $year, int $month, int $day, string|null $separator = null): string
+    {
+        return DateConversion::convert($year, $month, $day, $separator)->toEnglish();
+    }
+}
--- tests/DateConversionTest.php
+++ tests/DateConversionTest.php
@@ -11,3 +11,14 @@ it('converts to date in y/m/d format with custom separator', function () {
     $nepaliDate = DateConversion::convert(2022, 1, 17, '/')->toNepali();
     $this->assertEquals('2078/10/03', $nepaliDate);
 });
+
+it('converts date using helper function with default separator', function (){
+   $nepaliDate = toNepaliDate(2022, 1, 17);
+   $this->assertEquals('2078-10-03', $nepaliDate);
+});
+
+
+it('converts to date in y/m/d format with custom separator using helper function', function () {
+    $nepaliDate = toNepaliDate(2022, 1, 17, '/');
+    $this->assertEquals('2078/10/03', $nepaliDate);
+});
